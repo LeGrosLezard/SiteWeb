aca142c2b02e5dcdaaace00d35c226285833279a
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-07-24 21:28:13 +0200

    Update info_table.py
    jsp le like marche bon faut faire un jeu de donnée mtn mais trop la flemme....

--- projetto/compte/base_de_donnee/info_table.py
+++ projetto/compte/base_de_donnee/info_table.py
@@ -1,6 +1,108 @@
 import psycopg2
 
+from .CONFIG import DATABASE
+from .CONFIG import USER
+from .CONFIG import HOST
+from .CONFIG import PASSWORD
 
-def donnee():
-    pass
+
+def donnee_user(pseudo):
+        
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
+
+    cur = conn.cursor()
+    
+    cur.execute("""SELECT * from user
+                WHERE pseudo LIKE '%%s%'""", (pseudo, ))
+                       
+    
+    rows = cur.fetchall()
+    liste = [i for i in rows]
+
+
+    return liste
+
+
+
+
+
+
+def donnee_email(email):
+        
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
+
+    cur = conn.cursor()
+    
+    cur.execute("""SELECT * from user
+                WHERE email LIKE '%%s%'""", (email))
+                       
+    
+    rows = cur.fetchall()
+    liste = [i for i in rows]
+
+
+    return liste
+
+
+
+
+def donnee_telephone_portable(portable):
+        
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
+
+    cur = conn.cursor()
+    
+    cur.execute("""SELECT * from user
+                WHERE portable LIKE '%%s%'""", (portable))
+                       
+    
+    rows = cur.fetchall()
+    liste = [i for i in rows]
+
+
+    return liste
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
