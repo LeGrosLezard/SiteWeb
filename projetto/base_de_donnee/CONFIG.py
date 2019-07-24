0c4581e4aa8d6432b5fa8881b80cc211b3bf164c
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-07-24 21:34:04 +0200

    Delete CONFIG.py

--- projetto/base_de_donnee/CONFIG.py
+++
@@ -1,4 +0,0 @@
-DATABASE = "de9ed4bj77aj4h"
-USER = "pyrrjcrzmnejtt"
-HOST = "ec2-54-246-84-100.eu-west-1.compute.amazonaws.com"
-PASSWORD = "127e2ec10459fb1887ff8978eeef3d66b357c545cc0186758ae24e2f474df8c6"
