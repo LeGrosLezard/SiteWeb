b3edf649ac8a41f32909c654d95a2f99e2c06368
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-07-25 02:20:09 +0200

    Update and rename connection.py to connexion.py
    bon faudra aussi verifier tout ce bordel

---
+++ projetto/projetto/base_de_donnee/connexion.py
@@ -0,0 +1,115 @@
+import psycopg2
+
+from .CONFIG import DATABASE
+from .CONFIG import USER
+from .CONFIG import HOST
+from .CONFIG import PASSWORD
+
+
+
+def verifier_connexion(pseudo, password):
+
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
+
+    cur = conn.cursor()
+    
+    cur.execute("""SELECT pseudo, password FROM users
+                WHERE pseudo = '{0}' AND
+                password = '{1}'""".format(pseudo, password))
+                       
+
+    conn.commit() 
+
+    rows = cur.fetchall()
+    liste = [i for i in rows]
+
+
+    return liste
+
+
+def connexion_database(pseudo, password, GLOBAL_CONNEXION):
+
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
+
+    cur = conn.cursor()
+    
+    cur.execute("""INSERT INTO connexion
+                (pseudo, password)
+                values(%s, %s);""",
+                (pseudo, password))
+                       
+
+    conn.commit()
+
+
+    GLOBAL_CONNEXION.append(pseudo, password)
+
+
+
+
+
+
+def user_connected(GLOBAL_CONNEXION):
+
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
+
+    cur = conn.cursor()
+    
+    cur.execute("""SELECT pseudo, password FROM connexion
+                WHERE pseudo = '{0}' AND
+                password = '{1}'""".format(GLOBAL_CONNEXION[0], password[1]))
+                       
+
+    conn.commit() 
+
+    rows = cur.fetchall()
+    liste = [i for i in rows]
+
+
+    return liste
+
+
+
+
+
+
+
+
+def deconnexion_database(pseudo, password):
+
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
+
+    cur = conn.cursor()
+    
+    cur.execute("""INSERT INTO users
+                (nom, prenom, date_naissance, sexe, email,
+                fixe, password, pseudo, lieu_habitation, portable)
+                values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""",
+                (nom, prenom, date, sexe, email, fixe,
+                 password, pseudo, lieu_habitation, portable))
+                       
+
+    conn.commit()
+
+
+
+
+
+
+
+
+
+
+    
