4db15152ee47cbc1747b941236e3ee83d09b4c01
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-07-27 00:48:20 +0200

    Update questionnaire.py
    c = 0 normal putin 15 min a cherché pk ca marché pas

--- projetto/projetto/questionnaire/questionnaire.py
+++ projetto/projetto/questionnaire/questionnaire.py
@@ -1,37 +1,121 @@
-from .CONFIG import QUESTION
-def question_reponse(questionnaire):
+from .CONFIG import Ouverture_a_l_experience
+from .CONFIG import Conscience_professionnelle
+from .CONFIG import Extraversion
+from .CONFIG import Agreabilite
+from .CONFIG import Reactions_naturelles
 
-    questionnaire = questionnaire + "[]"
+
+
+def analyse_questionnaire(questionnaire):
+
     
-    nb = ""
-    liste1 = []
-    add = False
+    dico = {
+        "Imagination": 0,
+        "Interets_artistiques": 0,
+        "Profondeur_des_emotions" : 0,
+        "Volonte_experimenter" : 0,
+        "Curiosite_intellectuelle" : 0,
+        "Tolerance_a_la_diversite" : 0,
+        "Sens_des_competences" : 0,
+        "Ordre" : 0,
+        "Sens_des_responsabilites" : 0,
+        "Combativite_Recherche_de_resultats" : 0,
+        "Autodiscipline" : 0,
+        "Circonspection" : 0,
+        "Amenite" : 0,
+        "Sociabilite" : 0,
+        "Assertivite" : 0,
+        "Niveau_d_activite" : 0,
+        "Recherche_de_stimulation" : 0,
+        "emotions_positives" : 0,
+        "Confiance_en_autre"  : 0,
+        "Sincerite" : 0,
+        "Altruisme" : 0,
+        "Conformite" : 0,
+        "Modestie" : 0,
+        "Sympathie" : 0,
+        "Anxiete"  : 0,
+        "Colere_et_hostilite" : 0,
+        "Morosite_contentement"  : 0,
+        "Affectation" : 0,
+        "Indolence" : 0,
+        "Sensibilite_au_stress" : 0,
+    }
 
-    for i in questionnaire:
-        
-        if i == '"':
-            add = True
-        
-        elif i == ",":
-            if nb[1] == '"':
-                liste1.append(nb[2:-1])
-            else:
-                liste1.append(nb[1:-1])
-            nb = ""
 
-        if add is True and i not in ("[", "]", " "):
-            nb += i
+
+    dico_pts = {"a": 1, "b": 0.5, "c": 0, "d": -0.5, "e": -1}
 
 
     c1 = 0
     liste2 = []
-    for i in liste1:
-        if c1 % 2 == 0:
-            liste2.append([liste1[c1 - 1], liste1[c1]])
-
-        c1 += 1
+    for i in questionnaire:
         
-    return liste2
+        try:
+            liste2.append([liste1[c1], liste1[c1 + 1]])
+        except IndexError:
+            pass
+        
+        c1 += 2
+
+
+    for i in liste2:
+        #On parcours la liste nettoyé
+
+        for cle, valeur in QUESTION.items():
+            #On parcours la liste de question associé a la classe
+            #de personnalité
+
+            if cle == i[0]:
+                #on comapre le numéro de la question avec le numéro de la
+                #le type a répondu a la 52 -> la 52 = Ordre
+
+                for cle1, valeur1 in dico.items():
+                    if valeur == cle1:
+                        #Mtn on va scoriser
+                        #Du genre on cherche Ordre (global variable)
+                    
+                        for cle2, valeur2 in dico_pts.items():
+                            if i[1] == cle2:
+                                #on a la réponse sous forme a,b,c,d,e
+                                #ou a = 1 et e = -1
+                                
+                                dico[cle1] += int(valeur2)
+                                #On l'ajoute au dico
+        
+
+
+                    elif valeur[1:] == cle1:
+                        #Sinon si la phrase est tournée du genre
+                        #Je n'aime pas la poesie
+                        #Conteste curiosité art
+                        #si le mec dit oui tout a fait dac
+                        #on met -1 a ce truk
+                        #par contre s'il dit non pas du tout
+                        #a la base c du -1 donc ca devient du + 1 pour art
+                        for cle2, valeur2 in dico_pts.items():
+                            if i[1] == cle2:
+                                #on a la réponse sous forme a,b,c,d,e
+                                #ou a = 1 et e = -1
+                                
+                                dico[cle1] -= int(valeur2)
+                                #On l'ajoute au dico
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
