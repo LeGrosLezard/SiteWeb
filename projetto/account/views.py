80c495fd8ed910b1cc39c29e03a62b83367008da
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-07-28 14:44:47 +0200

    Update views.py

--- projetto/account/views.py
+++ projetto/account/views.py
@@ -49,36 +49,86 @@ def login_view(request):
     return render(request, 'login.html', context)
 
 
+from .compte.compte import inscription
+def register2(request):
+
+
+    if request.method == "POST":
+
+        inscriptionn = request.POST.get('inscriptionn')
+
+        if inscriptionn:
+
+            
+            current_user = request.user
+
+            user = User.objects.get(username=current_user)
+            password = user.password
+            email = user.email
+            pseudo = user.username
+
+            
+            nom = request.POST.get('nom')
+            prenom = request.POST.get('prenom')
+            date = request.POST.get('date')
+            sexe = request.POST.get('sexe')
+            portable = request.POST.get('portable')
+            fixe = request.POST.get('fixe')
+            adresse = request.POST.get('adresse')
+
+            info_inscription = inscription(pseudo,
+                                            nom, prenom,
+                                            date, sexe, email,
+                                            portable, fixe,
+                                            adresse,
+                                            str(password))
+
+
+
+        if next:
+            return redirect(next)
+        return redirect('/account/register2')
+
+    return render(request, 'register2.html',)
+
+
+
+                  
 
 def register(request):
     """Here we define the register view"""
 
+
     next = request.GET.get('next')
     form = UserRegisterForm(request.POST or None)
-    print("coucou")
-    
+
+
     if form.is_valid():
         
         print("coucou...")
         user = form.save(commit=False)
-        password = form.cleaned_data.get('password')
-        user.set_password(password)
+        passwordd = form.cleaned_data.get('password')
+        email = form.cleaned_data.get('email')
+        print(passwordd)
+        user.set_password(passwordd)
+        print(passwordd)
         user.save()
 
-
-        new_user = authenticate(username=user.username, password=password)
-
+        new_user = authenticate(username=user.username, password=passwordd)
         login(request, new_user)
+
 
         if next:
             return redirect(next)
-        return redirect('/')
+        return redirect('/account/register2')
 
     context = {
         'form':form
     }
 
     return render(request, 'register.html', context)
+
+
 
 
 login_required
