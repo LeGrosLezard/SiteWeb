96318ca57279e7872b19229cf1a19326e1f8fdc1
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-07-28 14:45:58 +0200

    Add files via upload
    okkkkkkkkkkkkk mtn faire tous les authenticate, ensuite les documents

---
+++ projetto/account/base_de_donnee/remplissage_user.py
@@ -0,0 +1,63 @@
+import psycopg2
+
+from .CONFIG import DATABASE
+from .CONFIG import USER
+from .CONFIG import HOST
+from .CONFIG import PASSWORD
+
+
+
+def insertion_user(nom, prenom, date, sexe, email, fixe, password,
+                   pseudo, lieu_habitation, portable):
+
+    print(nom, prenom, date, sexe, email, fixe, password,
+                   pseudo, lieu_habitation, portable)
+
+
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
+
+    cur = conn.cursor()
+    
+    cur.execute("""INSERT INTO users
+                (nom, prenom, date_naissance, sexe, email,
+                fixe, password, pseudo, lieu_habitation, portable)
+                values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""",
+                (nom, prenom, date, sexe, email, fixe,
+                 password, pseudo, lieu_habitation, portable))
+                       
+
+    conn.commit() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
