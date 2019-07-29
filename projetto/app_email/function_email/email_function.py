bbeccf0c10d3a050724b5ddf258a1b041faa14f6
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-07-29 22:30:15 +0200

    Update email_function.py
    merde j'ai écrit trop vite, j'ai la gerbe

--- projetto/app_email/function_email/email_function.py
+++ projetto/app_email/function_email/email_function.py
@@ -70,31 +70,55 @@ def pole_emploi(lieu, emploi, rayon):
     liste_w1 = []
     
     for i in propriete:
+        #Pour chaque élément trouvé cad chaque annonce,
+        #on cherche la suite(le détail de l'annonce)
+        #qui se traduit par href.......
+        
         liste_w = []
         liste_w.append(str(i.a))
+        #On récupere tous les href
+        
         url = ""
         
         for i in liste_w:
+            
             c = 0
             ok = False
+
+            
             for j in i:
+                #Pour chaque element de la div
+                
                 if i[c] == "h" and i[c + 1] == "r" and i[c + 2] == "e"\
                    and i[c + 3] == "f":
                     ok = True
                     c += 3
+                    #On cherche le mot href
+                    #si on le trouve on dit ok = True
+                    #qui permet la mise en liste et on ajout + 3
+                    #au counter (qui premet de nous balader dans un element)
                 
                 if ok is True:
                     if j == " ":
                         pass
+                    #Si j est un espace vide on l'ignore
+                    #comme ca on a pas l'espace dans notre url
                     else:
                         url += j
+                        #Sinon on ajoute j a notre url
                 
                 else:
                     c += 1
+                    #Si rien ne se produit, on increment la variable
 
                 if ok is True and j == " ":
                     liste_w1.append(url[6:-1])
                     break
+                #Si j est un espace (que l'on a ingoré dans notre incrémentaiton)
+                #On arrete tout, on
+                #ajoute a notre liste que l'on retourne l'url
+                #de 6 a -1 car on ignore les guillement de fin et
+                #le href du début (bie nque l'on est mis c + 3)
                 
 
 
