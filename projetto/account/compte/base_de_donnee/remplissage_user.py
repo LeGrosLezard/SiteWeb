6a3738e6da7cb170fff0cf00f792f51e86c81924
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-07-29 18:01:35 +0200

    Update remplissage_user.py

--- projetto/account/compte/base_de_donnee/remplissage_user.py
+++ projetto/account/compte/base_de_donnee/remplissage_user.py
@@ -29,23 +29,47 @@ def insertion_user(nom, prenom, date, sexe, email, fixe, password,
     conn.commit() 
 
 
+    
+    cur.execute("""SELECT id FROM users
+                where pseudo='{0}' """.format(pseudo))
+                       
+
+    conn.commit() 
+
+    rows = cur.fetchall()
+    user = [i for i in rows]
+    user = user[0][0]
+    
+
+    cur.execute("""INSERT INTO cv
+                (id_user, cv, cv1, cv2, cv3, cv4, cv5)
+                values(%s, '', '', '', '', '', '');""",(user, ))
+
+    conn.commit()
 
 
 
+    cur.execute("""INSERT INTO motivation
+                (id_user, lettre_motivation,
+                lettre_motivation1,
+                lettre_motivation2,
+                lettre_motivation3,
+                lettre_motivation4,
+                lettre_motivation5)
+                values(%s, '', '', '', '', '', '');""",(user, ))
 
+    conn.commit()
 
+    cur.execute("""INSERT INTO message
+                (id_user, lettre_motivation,
+                lettre_motivation1,
+                lettre_motivation2,
+                lettre_motivation3,
+                lettre_motivation4,
+                lettre_motivation5)
+                values(%s, '', '', '', '', '', '');""",(user, ))
 
-
-
-
-
-
-
-
-
-
-
-
+    conn.commit()
 
 
 
