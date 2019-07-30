eefe6af37f29f133c262cca99a7b7723acc4ccbf
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-07-31 01:19:29 +0200

    Update insertion_document_message.py

--- projetto/projetto/cv/database/insertion_document_message.py
+++ projetto/projetto/cv/database/insertion_document_message.py
@@ -30,7 +30,7 @@ def recuperation_id_pseudo(pseudo):
 
 
 def insertion_part_message(pseudo, part, mode):
-    """Here we insert into database the part cv."""
+    """Here we insert into database the part message."""
 
     id_user = recuperation_id_pseudo(pseudo)
     id_user = id_user[0][0]
@@ -57,7 +57,7 @@ def insertion_part_message(pseudo, part, mode):
     if mode == "deux":
 
         cur.execute("""UPDATE message
-                    SET lettre_motivation2 = '{1}'
+                    SET lettre_motivation1 = '{1}'
                     WHERE id_user = '{0}';""".format(id_user, part))
                            
 
@@ -68,7 +68,7 @@ def insertion_part_message(pseudo, part, mode):
     if mode == "trois":
 
         cur.execute("""UPDATE message
-                    SET lettre_motivation3 = '{1}'
+                    SET lettre_motivation2 = '{1}'
                     WHERE id_user = '{0}';""".format(id_user, part))
                            
 
@@ -79,7 +79,7 @@ def insertion_part_message(pseudo, part, mode):
     if mode == "quattre":
 
         cur.execute("""UPDATE message
-                    SET lettre_motivation4 = '{1}'
+                    SET lettre_motivation3 = '{1}'
                     WHERE id_user = '{0}';""".format(id_user, part))
                            
 
@@ -90,7 +90,7 @@ def insertion_part_message(pseudo, part, mode):
     if mode == "cinq":
 
         cur.execute("""UPDATE message
-                    SET lettre_motivation5 = '{1}'
+                    SET lettre_motivation4 = '{1}'
                     WHERE id_user = '{0}';""".format(id_user, part))
                            
 
@@ -101,7 +101,7 @@ def insertion_part_message(pseudo, part, mode):
     if mode == "six":
 
         cur.execute("""UPDATE message
-                    SET lettre_motivation6 = '{1}'
+                    SET lettre_motivation5 = '{1}'
                     WHERE id_user = '{0}';""".format(id_user, part))
                            
 
