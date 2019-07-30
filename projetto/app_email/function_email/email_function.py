c0fb914c60887e403b55d980808d3a32308d0231
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-07-30 21:11:35 +0200

    Update email_function.py
    bon ben je sais faire un cv sté pas si chiant en vrai !

--- projetto/app_email/function_email/email_function.py
+++ projetto/app_email/function_email/email_function.py
@@ -182,7 +182,13 @@ def verification_metier(liste, ville):
     #print(liste_sauvegarde)
     return liste_sauvegarde
 
+
+
+
 def recherche_email(path):
+    """Sur le site y'a un un dispositif a l'information
+    a gauche, on cherche si ya l'adresse email dans cet onglet"""
+
 
     request_html = requests.get(path)
     page = request_html.content
@@ -245,13 +251,107 @@ def recherche_email(path):
             #on trouve l'addresse mail
             #et on efface le retour de ligne
                 
-    print(liste_alpha_numeric1)
+
     if liste_alpha_numeric1 != []:
         return liste_alpha_numeric1, "adresse trouvée"
     else:
         return "", ""
 
 
+
+def recherche_via_image(path):
+    
+    request_html = requests.get(path)
+    page = request_html.content
+    soup_html = BeautifulSoup(page, "html.parser")
+    propriete = soup_html.findAll("div",{"class":"modal-apply"})
+
+    liste_propriete = []
+    liste_propriete.append(str(propriete))
+
+    c = 0
+    ok = False
+    url = ""
+    liste_final = []
+    
+    for i in liste_propriete:
+        
+        for j in i:
+            if i[c] == "h" and\
+               i[c + 1] == "r" and\
+               i[c + 2] == "e" and\
+               i[c + 3] == "f":
+                ok = True
+
+            if ok is True:
+                url += i[c]
+
+            if i[c] == " ":
+                liste_final.append(url)
+                url = ""
+                ok = False
+            c += 1
+
+
+    liste_final_return = []
+    for i in liste_final:
+        if i != "":
+            liste_final_return.append(i[6:-2])
+
+
+    return liste_final_return
+
+
+
+
+def recherche_appuie_image(path):
+    """On cherche qui a posté cette anonce"""
+
+    request_html = requests.get(path)
+    page = request_html.content
+    soup_html = BeautifulSoup(page, "html.parser")
+    propriete = soup_html.findAll("div",{"class":"bloc clear nm col-2 small-marge"})
+
+    liste_propriete = []
+    liste_propriete.append(str(propriete))
+
+
+    c = 0
+    entrprise = ""
+    ok = False
+    liste = []
+    for i in liste_propriete:
+ 
+        for j in i:
+            if i[c] == "p" and\
+               i[c + 1] == "o" and\
+               i[c + 2] == "s" and\
+               i[c + 3] == "t" and\
+               i[c + 4] == "é" and\
+               i[c + 5] == " " and\
+               i[c + 6] == "p" and\
+               i[c + 7] == "a" and\
+               i[c + 8] == "r" and\
+               i[c + 9] == " ":
+                ok = True
+
+            if ok is True:
+                entrprise += i[c]
+                
+            if i[c] == "<":
+                ok = False
+                liste.append(entrprise)
+
+            c += 1
+
+    return entrprise[10:-1]
+
+def recherche_entreprise_via_google(nom, ville):
+    pass
+
+def recherche_email_dans_description(path):
+    pass
+    
 def recherche_entreprise(liste):
     """On cherche l'entreprise mtn"""
 
@@ -263,20 +363,24 @@ def recherche_entreprise(liste):
 
         path = PATH_POLE_2.format(i[1][0])
 
+        
         email, trouvee = recherche_email(path)
-        print(email, trouvee)
+        #onglet info on cherche email
+
         if trouvee == "adresse trouvée":
             pass
         
         else:
+            recherche_par_image = recherche_via_image(path)
+            entreprise = recherche_appuie_image(recherche_par_image[0])
+            #si ya pas demail ya la société qui demande
+            #on click sur le href et on cherche la société
 
-            request_html = requests.get(path)
-            page = request_html.content
-            soup_html = BeautifulSoup(page, "html.parser")
-            propriete = soup_html.findAll("div",{"class":"modal-apply"})
 
-            for i in propriete:
-                print(i)
+
+
+
+
 
 
 
