8e3bce92fbb74c1f99d71c13a07d30f32f2423c6
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-01 00:22:51 +0200

    Update CONFIG.py

--- projetto/app_email/function_email/CONFIG.py
+++ projetto/app_email/function_email/CONFIG.py
@@ -1,3 +1,5 @@
 PATH_POSTAL = 'http://code.postal.fr/code-postal-{}'
 PATH_POLE = "https://candidat.pole-emploi.fr/offres/recherche?lieux={}&motsCles={}&offresPartenaires=true&range=0-100&rayon={}&tri=1"
 PATH_POLE_2 = "https://candidat.pole-emploi.fr{}"
+PATH_BING = "https://www.bing.com/search?q={0}+{1}+entreprise+contact+adresse+email&FORM=AWRE"
+PATH_GOOGLE = "https://www.google.com/search?q={0}+{1}+contact+mail&oq={0}&aqs=chrome.0.69i59j69i57j69i59l2j0j69i60.2255j0j7&sourceid=chrome&ie=UTF-8"
