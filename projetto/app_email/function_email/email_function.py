1646215cd3a874f4c800510c9338cb59f282cdd6
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-01 13:03:45 +0200

    Update email_function.py
    la technologie c dingue quand meme !

--- projetto/app_email/function_email/email_function.py
+++ projetto/app_email/function_email/email_function.py
@@ -7,6 +7,10 @@ msg['From'] = 'jeanbaptisteservais26@gmail.com'
 msg['To'] = 'jb26400@hotmail.fr'
 msg['Subject'] = 'Le sujet de mon mail' 
 message = 'Bonjour !'
+path = r'C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\app_email\function_email.py'
+msg.attach(MIMEText(path))
+
+
 
 msg.attach(MIMEText(message))
 mailserver = smtplib.SMTP('smtp.gmail.com', 587)
@@ -15,5 +19,8 @@ mailserver.starttls()
 mailserver.ehlo()
 
 mailserver.login('jeanbaptisteservais26@gmail.com', 'onsaitjamais')
-mailserver.sendmail('jb26400@hotmail.fr', 'jb26400@hotmail.fr', msg.as_string())
+
+mailserver.sendmail('jb26400@hotmail.fr',
+                    'jb26400@hotmail.fr',
+                    msg.as_string())
 mailserver.quit()
