3a89afe289efdb79f99534868daeddf521552b19
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-03 01:54:09 +0200

    Update remplissage_user.py
    merde

--- projetto/account/compte/base_de_donnee/remplissage_user.py
+++ projetto/account/compte/base_de_donnee/remplissage_user.py
@@ -5,6 +5,32 @@ from .CONFIG import USER
 from .CONFIG import HOST
 from .CONFIG import PASSWORD
 
+from .CONFIG import CV1
+from .CONFIG import CV2
+from .CONFIG import CV3
+from .CONFIG import CV4
+from .CONFIG import CV5
+from .CONFIG import CV6_1
+from .CONFIG import CV6_2
+from .CONFIG import CV6_3
+from .CONFIG import CV7
+from .CONFIG import METIER
+from .CONFIG import FORMATION
+from .CONFIG import POSTE1
+from .CONFIG import POSTE2
+from .CONFIG import POSTE3
+
+from .CONFIG import MOTIVATION1
+from .CONFIG import MOTIVATION2
+from .CONFIG import MOTIVATION3
+from .CONFIG import MOTIVATION4
+from .CONFIG import MOTIVATION5
+from .CONFIG import MOTIVATION6
+from .CONFIG import CODE
+from .CONFIG import VILLE
+from .CONFIG import POSTE_MOTIVATION
+
+
 
 
 def insertion_user(nom, prenom, date, sexe, email, fixe, password,
@@ -42,21 +68,50 @@ def insertion_user(nom, prenom, date, sexe, email, fixe, password,
     
 
     cur.execute("""INSERT INTO cv
-                (id_user, cv, cv1, cv2, cv3, cv4, cv5)
-                values(%s, '', '', '', '', '', '');""",(user, ))
+                (id_user, cv, cv1, cv2, cv3, cv4,
+                cv6_1, cv6_2, cv6_3, cv7, metier, formation, poste1,
+                poste2, poste3)
+                values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s,);""",(user,
+                                                                                CV1,
+                                                                                CV2,
+                                                                                CV3,
+                                                                                CV4,
+                                                                                CV5,
+                                                                                CV6_1,
+                                                                                CV6_2,
+                                                                                CV6_3,
+                                                                                CV7,
+                                                                                METIER,
+                                                                                FORMATION,
+                                                                                POSTE1,
+                                                                                POSTE2,
+                                                                                POSTE3))
 
     conn.commit()
 
 
 
     cur.execute("""INSERT INTO motivation
-                (id_user, lettre_motivation,
+                (id_user,
+                lettre_motivation,
                 lettre_motivation1,
                 lettre_motivation2,
                 lettre_motivation3,
                 lettre_motivation4,
-                lettre_motivation5)
-                values(%s, '', '', '', '', '', '');""",(user, ))
+                lettre_motivation5,
+                code,
+                ville,
+                poste_moitivation)
+                values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""",(user,
+                                                                    MOTIVATION1,
+                                                                    MOTIVATION2,
+                                                                    MOTIVATION3,
+                                                                    MOTIVATION4,
+                                                                    MOTIVATION5,
+                                                                    MOTIVATION6,
+                                                                    CODE,
+                                                                    VILLE,
+                                                                    POSTE_MOTIVATION))
 
     conn.commit()
 
