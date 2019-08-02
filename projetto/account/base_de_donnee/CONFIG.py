7610a2f892c8d9a729eafb6a5ae33090190edafa
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-02 15:34:28 +0200

    Update CONFIG.py

--- projetto/account/base_de_donnee/CONFIG.py
+++ projetto/account/base_de_donnee/CONFIG.py
@@ -2,3 +2,24 @@ DATABASE = "de9ed4bj77aj4h"
 USER = "pyrrjcrzmnejtt"
 HOST = "ec2-54-246-84-100.eu-west-1.compute.amazonaws.com"
 PASSWORD = "127e2ec10459fb1887ff8978eeef3d66b357c545cc0186758ae24e2f474df8c6"
+
+
+CV1 = "Bien décidé à commencé un nouveau travail, je cherche un emploi " +\
+      "ou je pourrai m'épanouir en apportant toutes les compétences que " + \
+      "j'ai acqui sur le métier de ...\n" +\
+      "Je suis quelqu'un de créatif, consciencieux, travailleur" +\
+      "et possédant une grande fléxibilité mentale."
+
+CV2 = "Livreur 5/10 \n Chanteur 6/10 \n Paint 5/10 \n Commercial 9/10"
+
+CV3 = "Windows 10, scène, semi-remorque, en équipe, en solo... "
+CV4 = "Je suis quelqu'un à l'écoute et s'implique à 100 % dans son travail. " +\
+      "Je suis sérieux et attentionné. " +\
+      "Par mon expérience de  vie suis très courageux et ne lâche jamais l'affaire. " +\
+      "Le stresse ne m'influence pas négativement dans le développement et " +\
+      "il peut s'avérer comme étant une source de concentration. " +\
+      "Je suis quelqu'un de discret."
+
+
+CV5 =""
+CV6 =""
