09dfb7f7b7de03c106fcb7cee3835129bc4a4045
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-05 14:31:42 +0200

    Update CONFIG.py
    va falloir mieux faire les config y'a des espaces

--- projetto/account/compte/base_de_donnee/CONFIG.py
+++ projetto/account/compte/base_de_donnee/CONFIG.py
@@ -4,22 +4,22 @@ HOST = "ec2-54-246-84-100.eu-west-1.compute.amazonaws.com"
 PASSWORD = "127e2ec10459fb1887ff8978eeef3d66b357c545cc0186758ae24e2f474df8c6"
 
 
-CV1 = "Bien décidé à commencé un nouveau travail, je cherche un emploi " +\
-      "ou je pourrai m'épanouir en apportant toutes les compétences que " + \
-      "j'ai acqui sur le métier de ...\n" +\
-      "Je suis quelqu'un de créatif, consciencieux, travailleur" +\
-      "et possédant une grande fléxibilité mentale."
+CV1 = """"Bien décidé à commencé un nouveau travail, je cherche un emploi  
+      ou je pourrai m'épanouir en apportant toutes les compétences que 
+      j'ai acqui sur le métier de ...\n 
+      Je suis quelqu'un de créatif, consciencieux, travailleur 
+      et possédant une grande fléxibilité mentale."""
 
 CV2 = "Livreur 5/10 \n Chanteur 6/10 \n Paint 5/10 \n Commercial 9/10"
 
 CV3 = "Windows 10, scène, semi-remorque, en équipe, en solo... "
 
-CV4 = "Je suis quelqu'un à l'écoute et s'implique à 100 % dans son travail. " +\
-      "Je suis sérieux et attentionné. " +\
-      "Par mon expérience de  vie suis très courageux et ne lâche jamais l'affaire. " +\
-      "Le stresse ne m'influence pas négativement dans le développement et " +\
-      "il peut s'avérer comme étant une source de concentration. " +\
-      "Je suis quelqu'un de discret."
+CV4 = """Je suis quelqu'un à l'écoute et s'implique à 100 % dans son travail. 
+      Je suis sérieux et attentionné. " 
+      Par mon expérience de  vie suis très courageux et ne lâche jamais l'affaire. 
+      Le stresse ne m'influence pas négativement dans le développement et 
+      il peut s'avérer comme étant une source de concentration. " 
+      Je suis quelqu'un de discret."""
 
 
 CV5 ="Date - établissement - Intitulé du diplome \n Date - établissement - Intitulé du diplome (du plus récent au plus vieux)"
@@ -47,21 +47,23 @@ POSTE_MOTIVATION = "Serveur au caffé la ristourne"
 
 MOTIVATION1 = "Etant actuellement à la recherche d’un emploi, je me permets de vous proposer ma candidature au poste de (emploi)."
             
-MOTIVATION2 = "1) - mon profil correspond à la description recherchée sur l’offre d’emploi\n" +\
-              "2) - Ma formation en patisserie m'a permis d'acquérir de nombreuses compétences parmi celles que vous recherchez.\n " +\
-              "3) - Je possède tous les atouts qui me permettront de réussir dans le rôle que vous voudrez bien me confier. Motivation, rigueur et écoute sont les maîtres mots de mon comportement professionnel."
+MOTIVATION2 = """1) - mon profil correspond à la description recherchée sur l’offre d’emploi\n
+              2) - Ma formation en patisserie m'a permis d'acquérir de nombreuses compétences parmi celles que vous recherchez.\n 
+              3) - Je possède tous les atouts qui me permettront de réussir dans le rôle que vous voudrez bien me confier. Motivation,
+              rigueur et écoute sont les maîtres mots de mon comportement professionnel."""
 
 
 
-MOTIVATION3 = "1) - Mon expérience en tant que plombier en entreprise Morin et x" +\
-              "m’a permis d’acquérir toutes les connaissances nécessaires à la bonne exécution des tâches du poste à pourvoir.\n" +\
-              "2) - Confronté régulièrement aux aléas du métier ou Confronté à tout type de coupe(pour les coiffeur) \n" +\
-              "3) - je suis capable de répondre aux imprévus en toute autonomie tout en gardant mon calme."
+MOTIVATION3 = """1) - Mon expérience en tant que plombier en entreprise Morin et x 
+              m’a permis d’acquérir toutes les connaissances nécessaires à la bonne exécution des tâches du poste à pourvoir.\n 
+              2) - Confronté régulièrement aux aléas du métier ou Confronté à tout type de coupe(pour les coiffeur) \n 
+              3) - je suis capable de répondre aux imprévus en toute autonomie tout en gardant mon calme."""
             
 
 MOTIVATION4 = "Intégrer votre entreprise représente pour moi un réel enjeu d’avenir dans lequel mon travail et mon honnêteté pourront s’exprimer pleinement."
 
-MOTIVATION5 = "Restant à votre disposition pour toute information complémentaire, je suis disponible pour vous rencontrer lors d’un entretien à votre convenance"
+MOTIVATION5 = """Restant à votre disposition pour toute information complémentaire,
+                je suis disponible pour vous rencontrer lors d’un entretien à votre convenance"""
 
 MOTIVATION6 = "Veuillez agréer, (Madame, Monsieur), l’expression de mes sincères salutations."
 
