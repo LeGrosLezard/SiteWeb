241b4f3228aeddf7ac81af6efaa5876b5a95c2d6
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-06 12:42:59 +0200

    Update document_user.py

--- projetto/projetto/cv/document_user.py
+++ projetto/projetto/cv/document_user.py
@@ -3,13 +3,28 @@ import shutil
 import time
 
 
-from CONFIG import PATH_FOLDER
+from .CONFIG import PATH_FOLDER
+
+
+def verification(pseudo, mode):
+
+    
+    path = PATH_FOLDER.format(pseudo)
+    
+    liste0 = os.listdir(path)
+    for i in liste0:
+        if i[:-4] == mode:
+            os.remove(i)
 
 
 
-from CONFIG import PATH_MEDIA_MOTIVATION
-from CONFIG import PATH_MEDIA_MOTIVATION_DOC
+    
+from .CONFIG import PATH_MEDIA_MOTIVATION
+from .CONFIG import PATH_MEDIA_MOTIVATION_DOC
 def document_motivation_download(pseudo):
+
+    
+    verification(pseudo, "motivation")
 
     liste = os.listdir(PATH_MEDIA_MOTIVATION)
     path = PATH_FOLDER.format(pseudo)
@@ -21,40 +36,26 @@ def document_motivation_download(pseudo):
             path_doc = PATH_MEDIA_MOTIVATION_DOC.format(i)
             shutil.move(path_doc, path)
 
-    os.chdir(path)
-    liste1 = os.listdir()
+
+    liste1 = os.listdir(path)
     for i in liste1:
         if i[:-4] == pseudo:
             os.rename(i, "motivation.pdf")
     
 
 
-from CONFIG import PATH_MEDIA_MOTIVATION
-from CONFIG import PATH_MEDIA_MOTIVATION_DOC
-def document_motivation_download(pseudo):
-
-    liste = os.listdir(PATH_MEDIA_MOTIVATION)
-    path = PATH_FOLDER.format(pseudo)
-
-    
-    for i in liste:
-        if i[:-4] == pseudo:
-
-            path_doc = PATH_MEDIA_MOTIVATION_DOC.format(i)
-            shutil.move(path_doc, path)
-
-    os.chdir(path)
-    liste1 = os.listdir()
-    for i in liste1:
-        if i[:-4] == pseudo:
-            os.rename(i, "motivation.pdf")
 
 
 
 
-from CONFIG import PATH_MEDIA_CV
-from CONFIG import PATH_MEDIA_CV_DOC
+
+
+from .CONFIG import PATH_MEDIA_CV
+from .CONFIG import PATH_MEDIA_CV_DOC
 def document_cv_download(pseudo):
+
+
+    verification(pseudo, "cv")
 
     liste = os.listdir(PATH_MEDIA_CV)
     path = PATH_FOLDER.format(pseudo)
@@ -66,17 +67,20 @@ def document_cv_download(pseudo):
             path_doc = PATH_MEDIA_CV_DOC.format(i)
             shutil.move(path_doc, path)
 
-    os.chdir(path)
-    liste1 = os.listdir()
+
+    liste1 = os.listdir(path)
     for i in liste1:
         if i[:-4] == pseudo:
             os.rename(i, "cv.pdf")
 
 
 
-from CONFIG import PATH_MEDIA_MESSAGE
-from CONFIG import PATH_MEDIA_MESSAGE_DOC
+from .CONFIG import PATH_MEDIA_MESSAGE
+from .CONFIG import PATH_MEDIA_MESSAGE_DOC
 def document_message_download(pseudo):
+
+
+    verification(pseudo, "message")
 
     liste = os.listdir(PATH_MEDIA_MESSAGE)
     path = PATH_FOLDER.format(pseudo)
@@ -88,16 +92,18 @@ def document_message_download(pseudo):
             path_doc = PATH_MEDIA_MESSAGE_DOC.format(i)
             shutil.move(path_doc, path)
 
-    os.chdir(path)
-    liste1 = os.listdir()
+    liste1 = os.listdir(path)
     for i in liste1:
         if i[:-4] == pseudo:
             os.rename(i, "message.pdf")
 
 
-from CONFIG import PATH_MEDIA_BILAN
-from CONFIG import PATH_MEDIA_BILAN_DOC
+from .CONFIG import PATH_MEDIA_BILAN
+from .CONFIG import PATH_MEDIA_BILAN_DOC
 def document_bilan_download(pseudo):
+
+
+    verification(pseudo, "bilan")
 
     liste = os.listdir(PATH_MEDIA_BILAN)
     path = PATH_FOLDER.format(pseudo)
@@ -109,8 +115,7 @@ def document_bilan_download(pseudo):
             path_doc = PATH_MEDIA_BILAN_DOC.format(i)
             shutil.move(path_doc, path)
 
-    os.chdir(path)
-    liste1 = os.listdir()
+    liste1 = os.listdir(path)
     for i in liste1:
         if i[:-4] == pseudo:
             os.rename(i, "bilan.pdf")
