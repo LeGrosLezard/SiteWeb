574ba52e59718605f51295a3b2239d6732066c2b
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-06 12:42:37 +0200

    Add files via upload

---
+++ projetto/projetto/cv/CONFIG.py
@@ -0,0 +1,17 @@
+PATH_FOLDER = r"C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\static\espace_user\{}"
+
+
+PATH_MEDIA_MOTIVATION = r"C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\projetto\media\Lettre de motivation"
+PATH_MEDIA_MOTIVATION_DOC = r"C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\projetto\media\Lettre de motivation\{}"
+
+
+PATH_MEDIA_CV = r"C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\projetto\media\cv"
+PATH_MEDIA_CV_DOC = r"C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\projetto\media\cv\{}"
+
+
+PATH_MEDIA_MESSAGE = r"C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\projetto\media\message"
+PATH_MEDIA_MESSAGE_DOC = r"C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\projetto\media\message\{}"
+
+
+PATH_MEDIA_BILAN = r"C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\projetto\media\bilan"
+PATH_MEDIA_BILAN_DOC = r"C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\projetto\media\bilan\{}"
