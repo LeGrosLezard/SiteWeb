cc8739949d395405cbd9d4835533585cb0aa7388
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-06 16:01:33 +0200

    Update document_user.py

--- projetto/projetto/cv/document_user.py
+++ projetto/projetto/cv/document_user.py
@@ -4,43 +4,46 @@ import time
 
 
 from .CONFIG import PATH_FOLDER
-
-
+from .CONFIG import PATH_FOLDER_CHANGE_NAME
 def verification(pseudo, mode):
 
     
     path = PATH_FOLDER.format(pseudo)
-    
     liste0 = os.listdir(path)
+    
     for i in liste0:
-        if i[:-4] == mode:
-            os.remove(i)
+        if str(i[:-4]) == mode:
+            path1 = PATH_FOLDER_CHANGE_NAME.format(str(pseudo), str(i))
+            os.remove(path1)
 
 
 
     
 from .CONFIG import PATH_MEDIA_MOTIVATION
 from .CONFIG import PATH_MEDIA_MOTIVATION_DOC
+from .CONFIG import PATH_FOLDER_CHANGE_NAME
 def document_motivation_download(pseudo):
 
-    
     verification(pseudo, "motivation")
 
     liste = os.listdir(PATH_MEDIA_MOTIVATION)
     path = PATH_FOLDER.format(pseudo)
-
     
     for i in liste:
-        if i[:-4] == pseudo:
-
+        if str(i[:-4]) == str(pseudo):
             path_doc = PATH_MEDIA_MOTIVATION_DOC.format(i)
             shutil.move(path_doc, path)
 
 
     liste1 = os.listdir(path)
+
+    
     for i in liste1:
-        if i[:-4] == pseudo:
-            os.rename(i, "motivation.pdf")
+        if str(i[:-4]) == str(pseudo):
+
+            path2 = PATH_FOLDER_CHANGE_NAME.format(str(pseudo), str(i))
+            path3 = PATH_FOLDER_CHANGE_NAME.format(str(pseudo), str("motivation.pdf"))
+            os.rename(path2, path3)
     
 
 
@@ -52,6 +55,7 @@ def document_motivation_download(pseudo):
 
 from .CONFIG import PATH_MEDIA_CV
 from .CONFIG import PATH_MEDIA_CV_DOC
+from .CONFIG import PATH_FOLDER_CHANGE_NAME
 def document_cv_download(pseudo):
 
 
@@ -62,7 +66,7 @@ def document_cv_download(pseudo):
 
     
     for i in liste:
-        if i[:-4] == pseudo:
+        if str(i[:-4]) == str(pseudo):
 
             path_doc = PATH_MEDIA_CV_DOC.format(i)
             shutil.move(path_doc, path)
@@ -70,13 +74,17 @@ def document_cv_download(pseudo):
 
     liste1 = os.listdir(path)
     for i in liste1:
-        if i[:-4] == pseudo:
-            os.rename(i, "cv.pdf")
+        if str(i[:-4]) == str(pseudo):
+
+            path2 = PATH_FOLDER_CHANGE_NAME.format(str(pseudo), str(i))
+            path3 = PATH_FOLDER_CHANGE_NAME.format(str(pseudo), str("cv.pdf"))
+            os.rename(path2, path3)
 
 
 
 from .CONFIG import PATH_MEDIA_MESSAGE
 from .CONFIG import PATH_MEDIA_MESSAGE_DOC
+from .CONFIG import PATH_FOLDER_CHANGE_NAME
 def document_message_download(pseudo):
 
 
@@ -87,19 +95,23 @@ def document_message_download(pseudo):
 
     
     for i in liste:
-        if i[:-4] == pseudo:
+        if str(i[:-4]) == str(pseudo):
 
             path_doc = PATH_MEDIA_MESSAGE_DOC.format(i)
             shutil.move(path_doc, path)
 
     liste1 = os.listdir(path)
     for i in liste1:
-        if i[:-4] == pseudo:
-            os.rename(i, "message.pdf")
+        if str(i[:-4]) == str(pseudo):
+
+            path2 = PATH_FOLDER_CHANGE_NAME.format(str(pseudo), str(i))
+            path3 = PATH_FOLDER_CHANGE_NAME.format(str(pseudo), str("message.pdf"))
+            os.rename(path2, path3)
 
 
 from .CONFIG import PATH_MEDIA_BILAN
 from .CONFIG import PATH_MEDIA_BILAN_DOC
+from .CONFIG import PATH_FOLDER_CHANGE_NAME
 def document_bilan_download(pseudo):
 
 
@@ -110,15 +122,18 @@ def document_bilan_download(pseudo):
 
     
     for i in liste:
-        if i[:-4] == pseudo:
+        if str(i[:-4]) == str(pseudo):
 
             path_doc = PATH_MEDIA_BILAN_DOC.format(i)
             shutil.move(path_doc, path)
 
     liste1 = os.listdir(path)
     for i in liste1:
-        if i[:-4] == pseudo:
-            os.rename(i, "bilan.pdf")
+        if str(i[:-4]) == str(pseudo):
+
+            path2 = PATH_FOLDER_CHANGE_NAME.format(str(pseudo), str(i))
+            path3 = PATH_FOLDER_CHANGE_NAME.format(str(pseudo), str("bilan.pdf"))
+            os.rename(path2, path3)
 
 
 
