da41945aa1c24e4d80bf013d4d0e85acb7bd8fea
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-06 22:16:09 +0200

    Update email_function.py
    bon plus qu'a faire, explique bien dans le message que c pas du spam et que t'as fais ce site pour montrer ton skill
    
    finir test ca c chiant
    
    réflechis pour cadju

--- projetto/app_email/function_email/email_function.py
+++ projetto/app_email/function_email/email_function.py
@@ -1,31 +1,63 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
-msg = MIMEMultipart()
-msg['From'] = 'jeanbaptisteservais26@gmail.com'
-msg['To'] = 'jb26400@hotmail.fr'
-msg['Subject'] = 'Le sujet de mon mail' 
-message = 'Bonjour !'
-path = r'C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\app_email\function_email\CONFIG.py'
-with open(path, "r") as fichier:
-    liste = fichier.read()
+from email.mime.multipart import MIMEBase
+from email import encoders
 
 
+def email(addresse, sujet,
+          message_recruteur,
+          path_cv, path_motivation):
+    
+    msg = MIMEMultipart()
+    msg['From'] = 'jeanbaptisteservais26@gmail.com'
+    msg['To'] = addresse
+    msg['Subject'] = sujet
+    message = message_recruteur
 
-msg.attach(MIMEText(message))
+
+    nom_fichier = "Curriculum vitae.pdf"
+    piece = open(path_cv, "rb")    ## Ouverture du fichier
+    part = MIMEBase('application', 'octet-stream')    
+    part.set_payload((piece).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "piece; filename= %s" % nom_fichier)
+    msg.attach(part)    
 
 
 
-msg.attach(MIMEText(liste))
-mailserver = smtplib.SMTP('smtp.gmail.com', 587)
-mailserver.ehlo()
-mailserver.starttls()
-mailserver.ehlo()
+    nom_fichier1 = "lettre de motivation.pdf"
+    piece1 = open(path_motivation, "rb")    ## Ouverture du fichier
+    part1 = MIMEBase('application', 'octet-stream')    
+    part1.set_payload((piece1).read())
+    encoders.encode_base64(part1)
+    part1.add_header('Content-Disposition', "piece; filename= %s" % nom_fichier1)
+    msg.attach(part1)    
 
-mailserver.login('jeanbaptisteservais26@gmail.com', 'onsaitjamais')
 
-mailserver.sendmail('jb26400@hotmail.fr',
-                    'jb26400@hotmail.fr',
-                    msg.as_string())
-mailserver.quit()
+    msg.attach(MIMEText(message))
+
+
+    mailserver = smtplib.SMTP('smtp.gmail.com', 587)
+    mailserver.ehlo()
+    mailserver.starttls()
+    mailserver.ehlo()
+
+    mailserver.login('jeanbaptisteservais26@gmail.com', 'onsaitjamais')
+
+    mailserver.sendmail('jb26400@hotmail.fr',
+                        'jb26400@hotmail.fr',
+                        msg.as_string())
+    mailserver.quit()
+
+
+
+
+
+
+
+
+
+
+
+
