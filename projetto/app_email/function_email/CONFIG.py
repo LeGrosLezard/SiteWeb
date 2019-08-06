17b20c04803645b8a891b7735fde18882cabdefb
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-06 22:38:12 +0200

    Update CONFIG.py
    ok faut que jdorme

--- projetto/app_email/function_email/CONFIG.py
+++ projetto/app_email/function_email/CONFIG.py
@@ -3,3 +3,7 @@ PATH_POLE = "https://candidat.pole-emploi.fr/offres/recherche?lieux={}&motsCles=
 PATH_POLE_2 = "https://candidat.pole-emploi.fr{}"
 PATH_BING = "https://www.bing.com/search?q={0}+{1}+entreprise+contact+adresse+email&FORM=AWRE"
 PATH_GOOGLE = "https://www.google.com/search?q={0}+{1}+contact+mail&oq={0}&aqs=chrome.0.69i59j69i57j69i59l2j0j69i60.2255j0j7&sourceid=chrome&ie=UTF-8"
+
+
+PATH_DOCUMENT = "C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\static\espace_user\{}\{}"
+
