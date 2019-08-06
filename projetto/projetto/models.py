a2a093de62ab12b79c1511f3f79616b70d744fb9
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-06 12:02:07 +0200

    Update models.py

--- projetto/projetto/models.py
+++ projetto/projetto/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class compte(models.Model):
 
     nom = models.CharField(max_length=255)
@@ -12,3 +13,11 @@ class compte(models.Model):
 
     def __str__(self):
         return self.email
+
+
+
+
+class Document(models.Model):
+    docfile = models.FileField(upload_to='Lettre de motivation')
+
+
