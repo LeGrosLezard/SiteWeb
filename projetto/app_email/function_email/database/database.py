d8493516589744106559420424df1a9f37ccba00
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-07 13:35:21 +0200

    Update database.py
    ok mtn message du mec

--- projetto/app_email/function_email/database/database.py
+++ projetto/app_email/function_email/database/database.py
@@ -14,26 +14,58 @@ def recup_user(pseudo):
 
     cur = conn.cursor()
 
-    cur.execute("""select pseudo, nom, prenom, addresse, from users;""")
+    cur.execute("""select id, nom, prenom, lieu_habitation, fixe,
+                portable, email from users;""")
 
     conn.commit() 
 
     rows = cur.fetchall()
     liste = [i for i in rows]
-
+    return liste
 
 def recup_message(pseudo):
+
+    id, nom, prenom, lieu_habitation, fixe,\
+                portable, email = recup_user(pseudo)
+
 
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
                             password=PASSWORD) 
 
+
     cur = conn.cursor()
 
-    cur.execute("""select * from users;""")
+    cur.execute("""select lettre_motivation,
+                            lettre_motivation1,
+                            lettre_motivation2,
+                            lettre_motivation3,
+                            lettre_motivation4,
+                            lettre_motivation5
+                            from message
+                            where id_user = {};""".format(id))
 
     conn.commit() 
 
     rows = cur.fetchall()
     liste = [i for i in rows]
+
+    return id, nom, prenom, lieu_habitation, fixe,\
+                portable, email, liste[0], liste[1],\
+                liste[2], liste[3], liste[4], liste[5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
