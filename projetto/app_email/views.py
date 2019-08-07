d887b3c39ee25df05e61981d59b2782557808133
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-07 14:01:23 +0200

    Update views.py

--- projetto/app_email/views.py
+++ projetto/app_email/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from .function_email.email_function import email
 from .function_email.CONFIG import PATH_DOCUMENT
 from .function_email.database.database import recup_message
+from .function_email.CONFIG import MESSAGE_MAIL
 def questionnaire_email(request):
 
     pseudo = request.user
@@ -31,7 +32,11 @@ def questionnaire_email(request):
 
             path_cv = PATH_DOCUMENT.format(pseudo, "cv.pdf")
             path_motivation = PATH_DOCUMENT.format(pseudo, "motivation.pdf")
-            message_recruteur = recup_message(pseudo)
+            msg = recup_message(pseudo)
+            message_recruteur = MESSAGE_MAIL.format(msg[0], msg[1], msg[6],
+                                                    msg[7], msg[8],
+                                                    msg[9], msg[10], msg[11],
+                                                    msg[2], msg[3], msg[4], a[5])
 
             for i in liste:
                 if i == "":
@@ -56,7 +61,11 @@ def questionnaire_email(request):
             liste = [metier1, metier2, metier3, metier4,
                      metier5, metier6, metier7]
             
-            message_recruteur = recup_message(pseudo)
+            msg = recup_message(pseudo)
+            message_recruteur = MESSAGE_MAIL.format(msg[0], msg[1], msg[6],
+                                                    msg[7], msg[8],
+                                                    msg[9], msg[10], msg[11],
+                                                    msg[2], msg[3], msg[4], a[5])
 
 
 
