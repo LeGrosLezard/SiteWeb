4894a1169fcf902cf50877fd5c0cfd2b65456a51
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-07 19:46:57 +0200

    Update questionnaire.py

--- projetto/projetto/questionnaire/questionnaire.py
+++ projetto/projetto/questionnaire/questionnaire.py
@@ -1,37 +1,25 @@
 from .CONFIG import QUESTION
 def question_reponse(questionnaire):
 
-    questionnaire = questionnaire + ", []"
-    
-    nb = ""
-    liste1 = []
-    add = False
 
+    liste = []
+    chiffre = ""
     for i in questionnaire:
-        
-        if i == '"':
-            add = True
-        
-        elif i == ",":
-            if nb[1] == '"':
-                liste1.append(nb[2:-1])
-            else:
-                liste1.append(nb[1:-1])
-            nb = ""
-
-        if add is True and i not in ("[", "]", " "):
-            nb += i
+        if i in ("'", '"', "[", ']', ","):
+            pass
+        else:
 
 
-    c1 = 0
-    liste2 = []
-    for i in liste1:
-        if c1 % 2 == 0:
-            liste2.append([liste1[c1], liste1[c1 - 1]])
+            try:
+                i = int(i)
+                if i == int(i):
+                    chiffre += str(i)
+            except:
+                liste.append([chiffre, i])
+                chiffre = ''
 
-        c1 += 1
-        
-    return liste2
+
+    return liste
 
 
 
