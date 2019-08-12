87301ad88561f6bdf643a9bf92eeb6b32b9408f4
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-12 14:35:45 +0200

    Update CONFIG.py
    refais les taleurs

--- projetto/projetto/questionnaire/CONFIG.py
+++ projetto/projetto/questionnaire/CONFIG.py
@@ -12,25 +12,25 @@ QUESTION = {"1": "Anxiete",
             "12": "Sens_des_competences",
             "13": "Profondeur_des_emotions",
             "14": "Altruisme",
-            "15": ",
+            "15": "",
             "16": "Sociabilite",
-            "17": ",
+            "17": "",
             "18": "Recherche_de_stimulation",
             "18": "Volonte_experimenter",
             "19": "Colere_et_hostilite",
             "20": "Combativite",
             "21": "Recherche_de_stimulation",
-            "22": ",
+            "22": "",
             "23": "Curiosite_intellectuelle ",
             "24": "Modestie",
             "25": "Circonspection",
             "26": "Sensibilite_au_stress",
-            "27": ",
+            "27": "",
             "28": "Conformite",
             "29": "Affectation",
             "31": "Anxiete",
             "32": "Sociabilite",
-            "33": ",
+            "33": "",
             "34": "Confiance_en_autre",
             "35": "Sens_des_responsabilites",
             "36": "Colere_et_hostilite",
@@ -39,7 +39,7 @@ QUESTION = {"1": "Anxiete",
             "39": "Indolence",
             "40": "Ordre",
             "41": "Morosite_contentement",
-            "41" ": "-emotions_positives",
+            "41": "-emotions_positives",
             "42": "Combativite_Recherche_de_resultats",
             "43": "Sympathie",
             "44": "Sympathie",
@@ -62,7 +62,7 @@ QUESTION = {"1": "Anxiete",
             "60": "-Circonspection",
             "61": "Anxiete",
             "62": "Sociabilite",
-            "63": ",
+            "63": "",
             "64": "Conformite",
             "65": "Sens_des_responsabilites",
             "66": "Colere_et_hostilite ",
@@ -72,24 +72,24 @@ QUESTION = {"1": "Anxiete",
             "70": "Ordre",
             "71": "emotions_positives",
             "72": "Circonspection",
-            "73": ",
+            "73": "",
             "74": "-Affectation",
-            "75": ",
+            "75": "",
             "76": "Sociabilite",
             "77": "Volonte_experimenter ",
             "78": "-Niveau_activite",
             "79": "Colere_et_hostilite",
             "80": "-Combativite_Recherche_de_resultats",
             "81": "Autodiscipline",
-            "82": ",
+            "82": "",
             "83": "-Curiosite_intellectuelle",
             "84": "Modestie",
-            "85": ",
+            "85": "",
             "86": "-Sens_des_competences",
             "87": "emotions_positives",
             "88": "Conformite",
             "89": "-Sympathie",
-            "90": ",
+            "90": "",
             "91": "Sensibilite_au_stress",
             "92": "-Sociabilite",
             "93": "Curiosite_intellectuelle ",
@@ -114,13 +114,13 @@ QUESTION = {"1": "Anxiete",
             "109": "Sympathie",
             "110": "Sens_des_responsabilites",
             "111": "Autodiscipline",
-            "112": ",
+            "112": "",
             "113": "-Curiosite_intellectuelle ",
             "114": "Modestie",
             "115": "Autodiscipline",
             "116": "Sensibilite_au_stress",
             "117": "emotions_positives",
-            "118": ",
+            "118": "",
             "119": "-Sympathie",
             "120": "-Circonspection",
         }
@@ -208,35 +208,35 @@ NOMBRE = {"Anxiete":4,
 
 
 #1 nom 2 il ou elle
-SPHERE = {"Ouverture_a_l_experience": ""{0} semble etre
+SPHERE = {"Ouverture_a_l_experience": """{0} semble etre
 une personne qui recherche activement
 et qui apprécie l'expérience en tant que telle.
 De plus, {1} possède une très grande imagination en plus du grande ouverture
 d'esprit. Travailler dans un cadre changeant n'est pas dérangeant et est
-meme une source de stimulation."",
+meme une source de stimulation.""",
           
-          "Conscience_professionnelle": ""{0} a un ahut degré d’organisation,
+          "Conscience_professionnelle": """{0} a un ahut degré d’organisation,
 de motivation et de minutie d’une personne dans
 sa vie et dans la poursuite de ses objectifs. 
-{0} est méthodiques, motivés et organisés."",
+{0} est méthodiques, motivés et organisés.""",
           
-          "Extraversion" ": ""L’extraversion décrit l’énergie et
+          "Extraversion": """L’extraversion décrit l’énergie et
 l’enthousiasme d’une personne dans ses
 rapports avec les gens. Elle fait référence aux
 préférences de la personne en matière de quantité et
 d’intensité des relations interpersonnelles. 
 Les scores élevés sont sociables et cherchent 
 activement la compagnie des autres. Plus le score 
-est élevé, plus la description fournie sera fidèle."",
+est élevé, plus la description fournie sera fidèle.""",
           
-            "Agréabilité" ": ""Le caractère agréable d'une personne fait
+            "Agréabilité": """Le caractère agréable d'une personne fait
 référence aux attitudes qu'elle adopte vis-à-vis des autres.
 Les personnes qui marquent un score élevé sont douées de compassion,
  sont franches et coopératives. Les personnes agréables possèdent une 
 vue optimiste de la nature humaine. Elles pensent que les gens sont
-foncièrement honnêtes, respectueux et dignes de confiance."",
+foncièrement honnêtes, respectueux et dignes de confiance.""",
           
-            "Réactions Naturelles" ": ""Les gens qui marquent un score 
+            "Réactions Naturelles": """Les gens qui marquent un score 
 élevé sont dotés de réactions naturelles fortes, excessivement
 sensibles et émotionnelles. Ils répondent de manière émotionnelle 
 aux événements qui n’affectent généralement pas beaucoup de personnes,
@@ -245,56 +245,56 @@ Ils sont plus enclins à interpréter les situations
 ordinaires comme menaçantes et les frustrations mineures comme de
 « gros problèmes ». Ces « réactions naturelles » fortes peuvent réduire
 la capacité d’une personne à penser clairement, à prendre des
-décisions et à gérer efficacement le stress.""
+décisions et à gérer efficacement le stress."""
             }
 
 
 
-SOUS_CLASSE = {"immagination": ""{0} a un sens poussé de l'immagination.
+SOUS_CLASSE = {"immagination": """{0} a un sens poussé de l'immagination.
 Sa capacité à immaginer lui permet de créer des mondes intérieurs
-puissants, riches et très interressant."",
+puissants, riches et très interressant.""",
                
-               "Interets_artistiques": ""Nous pouvons confirmer que {0} a un
+               "Interets_artistiques": """Nous pouvons confirmer que {0} a un
 senes poussé vers les intérêts artistiques.
 Ces personnes se laissent facilement absorber
-par les événements artistiques et naturels."",
+par les événements artistiques et naturels.""",
                
-               "Profondeur_des_emotions": ""{0} sollicitent et perçoivent
-facilement leurs propres sentiments. "",
+               "Profondeur_des_emotions": """{0} sollicitent et perçoivent
+facilement leurs propres sentiments. """,
                
-                "Volonte_expérimenter": ""{0} aime découvrir de nouvelles 
+                "Volonte_expérimenter": """{0} aime découvrir de nouvelles 
 activités, à voyager à l’étranger,
 à expérimenter de nouvelles choses.
 La familiarité et la routine les ennuient.
-Cet aspect de personnalité est un vrai atout."",
+Cet aspect de personnalité est un vrai atout.""",
                
-                "Curiosite_intellectuelle": ""{0} appréci de jongler
+                "Curiosite_intellectuelle": """{0} appréci de jongler
 avec les idées  ouverts aux idées nouvelles et
 inhabituelles et apprécient les débats et
 questionnements intellectuels. Il apprécie
-les problèmes, réflexions, puzzles et énigmes de l’esprit."",
+les problèmes, réflexions, puzzles et énigmes de l’esprit.""",
                
-                "Tolerance_a_la_diversite":  ""{0} accepte l’autorité et
+                "Tolerance_a_la_diversite":  """{0} accepte l’autorité et
 préfèrent la stabilité
-et la sécurité de la conformité à la tradition."",
+et la sécurité de la conformité à la tradition.""",
                
-                "Sens_des_competences" ": ""Le sens des compétences décrit la confiance
+                "Sens_des_competences": """Le sens des compétences décrit la confiance
 que la personne a en sa capacité d’accomplir les choses. Les scores élevés
 pensent qu’ils possèdent l’intelligence (le bon sens), la force 
 et la maîtrise nécessaires pour réussir. Ils ont un niveau élevé
-d’estime de soi et ont tendance à se sentir préparés pour affronter la vie"",
+d’estime de soi et ont tendance à se sentir préparés pour affronter la vie""",
                
-                        "Ordre" ": ""Les scores élevés sont des personnes très organisées,
+                        "Ordre": """Les scores élevés sont des personnes très organisées,
 ordonnées et propres. Elles aiment la routine et une vie programmée.
-Elles tiennent des listes et planifient leur vie"",
+Elles tiennent des listes et planifient leur vie""",
                
-                        "Sens_des_responsabilites" ": ""Cette facette reflète la force,
+                        "Sens_des_responsabilites" : """Cette facette reflète la force,
 chez une personne, de son sens du devoir et de l’obligation.
 Les scores élevés possèdent un solide sens de l’obligation morale.
 Ces personnes tendent à avoir un sens aigu de
-l’éthique et des principes stricts."",
+l’éthique et des principes stricts.""",
                
-                        "Combativite_Recherche_de_resultats" ": ""Les gens qui ont un score 
+                        "Combativite_Recherche_de_resultats": """Les gens qui ont un score 
 élevé pour cette facette se battent pour atteindre l’excellence. Leur
 combat pour que leurs succès soient reconnus les tient toujours en haleine
 ; ils travaillent dur pour atteindre leurs objectifs. Ils
@@ -305,103 +305,103 @@ accros du travail. Les scores faibles ne sont pas poussés par
 la nécessité de réussir ; ils ont tendance à se contenter
 d’un minimum d’efforts et de travail. Bien qu’ils puissent
 ne pas éprouver la nécessité de réussir, ils sont souvent
-très heureux des résultats qu’ils obtiennent."",
+très heureux des résultats qu’ils obtiennent.""",
                
-                        "Autodiscipline" ": ""L’autodiscipline, souvent appelée 
+                        "Autodiscipline": """L’autodiscipline, souvent appelée 
 « force de volonté », est définie comme l’habileté à 
 persister dans des tâches difficiles ou déplaisantes 
 jusqu’à ce qu’elles soient terminées. Les personnes 
 ayant un niveau élevé d’autodiscipline sont capables de surmonter leur répugnance à entamer des tâches et de ne pas
 succomber aux distractions. 
-Elles sont mues par une forte motivation à terminer leur travail."",
+Elles sont mues par une forte motivation à terminer leur travail.""",
                
-                        "Circonspection" ": ""La circonspection décrit la disposition à réfléchir soigneusement
+                        "Circonspection" : """La circonspection décrit la disposition à réfléchir soigneusement
 à toutes les options possibles avant d’agir.
 Les scores élevés pour la facette
-Circonspection sont minutieux et prennent leur temps avant de se décider."",
+Circonspection sont minutieux et prennent leur temps avant de se décider.""",
                
-                        "Amenite" ": ""Les scores élevés aiment
+                        "Amenite" : """Les scores élevés aiment
 naturellement parler avec les autres et expriment
 ouvertement des sentiments positifs à l’égard des autres.
-Ils se lient vite d’amitié et il leur est facile de nouer des relations intimes et proches."",
+Ils se lient vite d’amitié et il leur est facile de nouer des relations intimes et proches.""",
                
-                        "Sociabilite" ": ""Les gens sociables trouvent la
+                        "Sociabilite" : """Les gens sociables trouvent la
 compagnie des autres plaisante, stimulante et valorisante.
-Ils apprécient l’excitation provoquée par les foules."",
+Ils apprécient l’excitation provoquée par les foules.""",
                
-                        "Assertivite" ": ""Les scores élevés en Assertivité apprécient
+                        "Assertivite" : """Les scores élevés en Assertivité apprécient
 de prendre la parole, de prendre en charge et diriger les activités des autres.
-Ils ont tendance à être les meneurs dans les groupes."",
+Ils ont tendance à être les meneurs dans les groupes.""",
                
-                        "Niveau_activite" ": ""Les personnes actives mènent une vie
+                        "Niveau_activite" : """Les personnes actives mènent une vie
 rythmée et occupée. Ils sont toujours dans l’action, leurs gestes
-sont rapides, ils sont dynamiques, vigoureux et s’impliquent dans de nombreuses activités."",
+sont rapides, ils sont dynamiques, vigoureux et s’impliquent dans de nombreuses activités.""",
                
-                        "Recherche_de_stimulation" ": ""Les scores élevés pour cette
+                        "Recherche_de_stimulation" : """Les scores élevés pour cette
 facette de personnalité s’ennuient facilement si le taux de stimulation
 n’est pas important. Ils apprécient l’éclat, le tourbillon d’activités.
-Ils ont tendance à prendre des risques et à chercher les frissons."",
+Ils ont tendance à prendre des risques et à chercher les frissons.""",
                
-                        "Emotions_positives" ": ""Cette facette mesure les humeurs et
+                        "Emotions_positives" : """Cette facette mesure les humeurs et
 sentiments positifs. Les gens qui marquent un score élevé éprouvent
 généralement une palette de sentiments positifs dont l’allégresse,
-l’enthousiasme, l’optimisme et la joie."",
+l’enthousiasme, l’optimisme et la joie.""",
                
-                        "Confiance_en_autre" ": ""Les personnes à score élevé dans
+                        "Confiance_en_autre" : """Les personnes à score élevé dans
 cette facette estiment fondamentalement que les gens sont en
 général honnêtes, francs et bien intentionnés. Elles jugent
-les gens sur leur apparence et sont prompts à pardonner et à oublier."",
+les gens sur leur apparence et sont prompts à pardonner et à oublier.""",
                
-                        "Sincerite" ": ""Les scores élevés pour cette facette ne ressentent
+                        "Sincerite" : """Les scores élevés pour cette facette ne ressentent
 pas le besoin de faire semblant ou de manipuler dans les
-relations aux autres et sont dès lors candides, francs et authentiques. "",
+relations aux autres et sont dès lors candides, francs et authentiques. """,
                
-                        "Altruisme" ": ""Les gens altruistes trouvent une réelle valorisation
+                        "Altruisme" : """Les gens altruistes trouvent une réelle valorisation
 dans le fait d’aider les autres. Dès lors, ils apprécient généralement
 d’aider les gens dans le besoin. Les gens altruistes estiment qu’aider
-les autres est une forme d’auto-accomplissement plutôt que de sacrifice."",
+les autres est une forme d’auto-accomplissement plutôt que de sacrifice.""",
                
-                        "Conformite" ": ""Les scores élevés dans cette facette concernent
+                        "Conformite": """Les scores élevés dans cette facette concernent
 des gens qui n’aiment pas les confrontations. Ils sont parfaitement
 disposés à compromettre ou à nier leurs propres
-besoins pour bien s’entendre avec les autres."",
+besoins pour bien s’entendre avec les autres.""",
                
-                        "Modestie" ": ""Les scores élevés sont des
+                        "Modestie" : """Les scores élevés sont des
 gens modestes, plutôt effacés et humbles. Il est 
 toutefois important de comprendre qu’ils ne manquent 
-pas pour autant de confiance en soi ni d’estime de soi."",
+pas pour autant de confiance en soi ni d’estime de soi.""",
                
-                        "Sympathie" ": ""Les scores élevés sont des
+                        "Sympathie" : """Les scores élevés sont des
 personnes au cœur tendre et compatissantes. Elles éprouvent
-viscéralement la peine des autres, sont empathiques et facilement pétries de pitié."",
+viscéralement la peine des autres, sont empathiques et facilement pétries de pitié.""",
                
-                        "Anxiete" ": ""généralement décontractés, calmes et
-sans peur. Ils ont tendance à ne pas s’effrayer lorsque les choses pourraient mal tourner."",
+                        "Anxiete" : """généralement décontractés, calmes et
+sans peur. Ils ont tendance à ne pas s’effrayer lorsque les choses pourraient mal tourner.""",
                
-                        "Colere_et_hostilite" ": ""sont plus souples et moins
-susceptibles d’être irrités. Ils ne se fâchent pas souvent ni facilement."",
+                        "Colere_et_hostilite" : """sont plus souples et moins
+susceptibles d’être irrités. Ils ne se fâchent pas souvent ni facilement.""",
                
-                        "Morosite_contentement": ""Les gens qui ont un
+                        "Morosite_contentement": """Les gens qui ont un
 score bas sont moins susceptibles d’éprouver ces émotions.
 Cela ne veut pas dire pour autant qu’ils sont plus vigoureux,
-plus allègres ou légers (ces comportements-là étant associés à l’extraversion)."",
+plus allègres ou légers (ces comportements-là étant associés à l’extraversion).""",
                
-                        "Affectation": ""Par contraste, les scores
+                        "Affectation": """Par contraste, les scores
 faibles ne souffrent pas de cette fausse impression que tout
 le monde les regarde et les juge. Ils ne sont pas nécessairement 
-dotés de bonnes aptitudes sociales mais ils se sentent simplement moins nerveux en société."",
+dotés de bonnes aptitudes sociales mais ils se sentent simplement moins nerveux en société.""",
                
-                        "Indolence":  ""Les scores bas n’éprouvent pas de besoins fulgurants,
-irrésistibles et, par conséquent, ne sont pas tentés par les excès."",
+                        "Indolence":  """Les scores bas n’éprouvent pas de besoins fulgurants,
+irrésistibles et, par conséquent, ne sont pas tentés par les excès.""",
                
-                        "Sensibilite_au_stress": ""Les scores bas se sentent
+                        "Sensibilite_au_stress": """Les scores bas se sentent
 plus capables et aptes à ne pas perdre leur
-sang-froid en situation difficile ou sous la pression.""
+sang-froid en situation difficile ou sous la pression."""
                         }
 
 
 
-DICTEE =""il était une heure du matin. Une énorme déflagration s'est fait entendre.
+DICTEE ="""il était une heure du matin. Une énorme déflagration s'est fait entendre.
 qu'elle panique il y a eu ! Les passagers se sont tous précipités sur le pont à la demande
 du capitaine. Mais les membres de l'équipage semblaient affolés.
 Ils les ont entendu hurler. Et les ont vus préparer les bateaux de survie.
@@ -409,7 +409,7 @@ Pris de panique, les passagers se sont vus mourrir.
 Ils se sont finalement laissé convaincre de quitter le navire dans le calme.
 Ils sont montés dans les chaloupes et se sont tous fais ramener sain et sauf
 sur la côte.
-""
+"""
 
 
 
