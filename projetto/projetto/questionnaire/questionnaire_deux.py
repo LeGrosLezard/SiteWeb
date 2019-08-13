b73e1f0870e46d9a3b5b72baae9f2dd8459e7286
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-13 14:42:13 +0200

    Update questionnaire_deux.py
    faut faire la condition ou il rempli pas

--- projetto/projetto/questionnaire/questionnaire_deux.py
+++ projetto/projetto/questionnaire/questionnaire_deux.py
@@ -6,6 +6,7 @@ def traitement_DICTEE(DICTEE):
     for i in DICTEE:
         liste.append(i.lower())
 
+    
     return liste
 
 
@@ -13,22 +14,43 @@ def traitement_DICTEE(DICTEE):
 def traitement_texte_utilisateur(texte, dictee):
     liste = []
     texte = texte.split()
-    for i in texte:
-        liste.append(i.lower())
 
-    try:
-
-        c = 0
-        faute = 0
-        for i in dictee:
-            if i != liste[c]:
-                faute += 1
-            c += 1
-            
-    except IndexError:
+    if len(texte) < 50:
+        #return None
         pass
-    
-    return faute
+    else:
+        for i in texte:
+            liste.append(i.lower())
+
+        try:
+
+            c = 0
+            faute = 0
+            for i in dictee:
+                if i != liste[c]:
+                    faute += 1
+                c += 1
+                
+        except IndexError:
+            pass
+
+
+        if faute < 10:
+            out = "La personne a moins de 10 erreurs. Elle est ok ect faire le truk pk langage important"
+
+        else:
+            out =  None
+
+        return out
+
+
+
+
+
+
+
+
+        
 
 
 
