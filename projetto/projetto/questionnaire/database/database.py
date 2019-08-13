73e1199ba4f292bda9433f99c42c93a286320136
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-13 14:25:14 +0200

    Update database.py

--- projetto/projetto/questionnaire/database/database.py
+++ projetto/projetto/questionnaire/database/database.py
@@ -6,8 +6,34 @@ from .CONFIG import HOST
 from .CONFIG import PASSWORD
 
 
+
+
+def recuperation_id_pseudo(pseudo):
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
+
+    cur = conn.cursor()
+
+    cur.execute("""select id from users
+                WHERE (pseudo = '{0}');""".format(pseudo))
+                       
+
+    conn.commit()  
+
+    rows = cur.fetchall()
+    liste = [i for i in rows]
+
+
+    return liste
+
+
 def insertion_bilan_premiere_partie(pseudo, bilan):
 
+
+    id_user = recuperation_id_pseudo(pseudo)
+    id_user = id_user[0][0]
 
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
@@ -16,9 +42,9 @@ def insertion_bilan_premiere_partie(pseudo, bilan):
 
     cur = conn.cursor()
     
-    cur.execute("""UPDATE bilan
-                set bilan = %s
-                WHERE id_user = {0};""".format(user, (bilan, )))
+    cur.execute("""INSERT INTO bilan
+                (id_user, bilan)
+                VALUES(%s, %s);""", (id_user, bilan))
                        
 
     conn.commit() 
