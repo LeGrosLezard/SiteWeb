cc01b48cc817394b5b63ed2746620f05356e64b6
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-16 15:30:25 +0200

    Update questionnaire_trois.py

--- projetto/projetto/questionnaire/questionnaire_trois.py
+++ projetto/projetto/questionnaire/questionnaire_trois.py
@@ -1,30 +1,35 @@
 from .CONFIG import questionnaire_3
 
 def resultat_function(resultat):
-    
+
     liste = []
     nb = ""
     for i in resultat:
+
         if i == ",":
             try:
                 liste.append(int(nb))
+                nb = ""
             except ValueError:
                 liste.append(0)
-            nb = ""
+            
         else:
             nb += i
 
-
     return liste
 
-def correction(liste):
+def correction_questionnaire3(liste):
+
     c = 0
     counter = 0
 
     for i in liste:
 
-        if i == questionnaire_3[c]:
+        if int(i[0]) == questionnaire_3[c]:
             counter += 1
         c+=1
 
     return counter
+
+
+
