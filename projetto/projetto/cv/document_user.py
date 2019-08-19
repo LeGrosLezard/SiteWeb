3eaf444eeeb364a2c7ca74c8c5a6ec0fa403a424
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-19 16:32:21 +0200

    Update document_user.py

--- projetto/projetto/cv/document_user.py
+++ projetto/projetto/cv/document_user.py
@@ -66,6 +66,7 @@ def document_cv_download(pseudo):
 
     
     for i in liste:
+        print(i)
         if str(i[:-4]) == str(pseudo):
 
             path_doc = PATH_MEDIA_CV_DOC.format(i)
@@ -109,31 +110,40 @@ def document_message_download(pseudo):
             os.rename(path2, path3)
 
 
+
+
+
+from .CONFIG import PATH_FOLDER
+from .CONFIG import PATH_FOLDER_CHANGE_NAME
+
+def verification_bilan(pseudo, nom_fichier):
+    path = PATH_FOLDER.format(pseudo)
+    liste0 = os.listdir(path)
+    for i in liste0:
+        if i == nom_fichier:
+            os.remove(PATH_FOLDER_CHANGE_NAME.format(pseudo, i))
+
+
+
+
+
+
+
+
 from .CONFIG import PATH_MEDIA_BILAN
 from .CONFIG import PATH_MEDIA_BILAN_DOC
 from .CONFIG import PATH_FOLDER_CHANGE_NAME
-def document_bilan_download(pseudo):
+def document_bilan_download(pseudo, numero_bilan):
 
+    verification_bilan(pseudo, numero_bilan)
+    
+    liste0 = os.listdir(PATH_MEDIA_BILAN)
 
-    verification(pseudo, "bilan")
-
-    liste = os.listdir(PATH_MEDIA_BILAN)
     path = PATH_FOLDER.format(pseudo)
 
-    
-    for i in liste:
-        if str(i[:-4]) == str(pseudo):
-
-            path_doc = PATH_MEDIA_BILAN_DOC.format(i)
-            shutil.move(path_doc, path)
-
-    liste1 = os.listdir(path)
-    for i in liste1:
-        if str(i[:-4]) == str(pseudo):
-
-            path2 = PATH_FOLDER_CHANGE_NAME.format(str(pseudo), str(i))
-            path3 = PATH_FOLDER_CHANGE_NAME.format(str(pseudo), str("bilan.pdf"))
-            os.rename(path2, path3)
+    for i in liste0:
+        if i == numero_bilan:
+            shutil.move(PATH_MEDIA_BILAN_DOC.format(i), path)
 
 
 
