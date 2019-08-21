45adb97b16ee483452923287ba4121124c74e00e
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-21 15:31:26 +0200

    Update CONFIG.py
    ok pluqua faire la suite, faire une recherche DE TOUT LES ARRONDISSEMENTS sinon juste un

--- projetto/app_email/function_email/CONFIG.py
+++ projetto/app_email/function_email/CONFIG.py
@@ -1,4 +1,6 @@
-PATH_POSTAL = 'http://code.postal.fr/code-postal-{}'
+PATH_POSTAL ="https://www.google.com/search?q=code+insee+{}"
+
+
 PATH_POLE = "https://candidat.pole-emploi.fr/offres/recherche?lieux={}&motsCles={}&offresPartenaires=true&range=0-100&rayon={}&tri=1"
 PATH_POLE_2 = "https://candidat.pole-emploi.fr{}"
 PATH_BING = "https://www.bing.com/search?q={0}+{1}+entreprise+contact+adresse+email&FORM=AWRE"
@@ -8,13 +10,12 @@ PATH_GOOGLE = "https://www.google.com/search?q={0}+{1}+contact+mail&oq={0}&aqs=c
 PATH_DOCUMENT = r"C:\Users\jeanbaptiste\Desktop\site_travail\environement_virtuel\projetto\static\espace_user\{}\{}"
 
 MESSAGE_MAIL = """{0} {1} \n
-{2}\n
-{3}\n
-{4}\n
-{5}\n
-{6}\n
-{7}\n\n
-
+{2}
+{3}
+{4}
+{5}
+{6}
+{7}
 
 Contacte de {0} {1}:\n
 adresse :{8}
@@ -26,5 +27,31 @@ Ce message vous est envoyé par Projetto un site web opensource
 qui a pour but d'aider les personnes en recherche d'emploi
 à trouver un travail plus vite. Pour plus d'information nous contacter
 à l'adresse suivant: jeanbaptisteservais26@gmail.com
+
+https://candidat.pole-emploi.fr/offres/recherche?lieux=69381&motsCles=d%C3%A9veloppeur+web+python&offresPartenaires=true&range=0-9&rayon=20&tri=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 """
